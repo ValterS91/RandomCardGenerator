b74b0fb9eb0bb93db3236edd3d0437b7cbb1b609
ValterS <valters18@gmail.com>
2021-10-19 13:54:16 +0000

    Finished Repo Upload

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const BODY = document.querySelector("body");
+const ICON = ["♦", "♥", "♠", "♣"];
+const NUMBER = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let randomICON = Math.floor(Math.random() * ICON.length);
+let randomNumber = Math.floor(Math.random() * NUMBER.length);
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  createCard();
 };
+
+function createCard() {
+  let card = document.createElement("div");
+  card.classList.add("card");
+  BODY.appendChild(card);
+
+  let TOP = document.createElement("div");
+  TOP.classList.add("card-topsuite");
+  TOP.innerHTML = ICON[randomICON];
+  card.appendChild(TOP);
+
+  let CENTER = document.createElement("div");
+  CENTER.classList.add("card-centersuite");
+  CENTER.innerHTML = NUMBER[randomNumber];
+  card.appendChild(CENTER);
+
+  let BOTTOM = document.createElement("div");
+  BOTTOM.classList.add("card-bottomsuite");
+  BOTTOM.innerHTML = ICON[randomICON];
+  card.appendChild(BOTTOM);
+
+  if (ICON[randomICON] == "♦" || ICON[randomICON] == "♥") {
+    TOP.classList.add("color-red");
+    BOTTOM.classList.add("color-red");
+  } else {
+    if (ICON[randomICON] == "♠" || ICON[randomICON] == "♣") {
+      TOP.classList.add("color-black");
+      BOTTOM.classList.add("color-black");
+    }
+  }
+}
